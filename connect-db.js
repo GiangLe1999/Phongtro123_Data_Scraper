eeb4ea80cf29bda4a8b9481108b7282ba8f67bbb
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-09-26 16:47:04 +0700

    Update

--- connect-db.js
+++ connect-db.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
+  port: process.env.DB_PORT.toString(),
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-require("dotenv").config()(
-  // Invoked Function Expression to call the asynchronous function immediately
-  async () => {
-    const startBrowser = require("./browser");
-    const scrapeController = require("./scrapeController");
+// Invoked Function Expression to call the asynchronous function immediately
+(async () => {
+  const startBrowser = require("./browser");
+  const scrapeController = require("./scrapeController");
 
-    let browser = await startBrowser();
-    await scrapeController(browser);
-  }
-)();
+  let browser = await startBrowser();
+  await scrapeController(browser);
+})();
